2cc223757b59fa196e70f94af9856913e9e46d51
boyhoodbaby <boyhoodbaby@163.com>
2020-08-09 14:40:35 +0800

    add BUG

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Login_container">
+        登录组件
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.Login_container{
+    background-color: #2b4b6b;
+}
+</style>
