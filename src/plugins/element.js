4f86d169ddac0fedbf0f1d98dfdf16703d98aa15
boyhoodbaby <boyhoodbaby@163.com>
2020-08-11 19:56:45 +0800

    完成了权限功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input, message, Container, Header, Aside, Main, Menu,
   Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col,
-  Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox
+  Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree,
+  Select, Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -28,5 +29,9 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message = message
 Vue.prototype.$confirm = MessageBox.confirm
