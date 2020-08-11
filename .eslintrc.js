7856a49101b4942a7e6331942bb1269043dbe4f9
boyhoodbaby <boyhoodbaby@163.com>
2020-08-11 10:30:03 +0800

    完成用户列表的功能的开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    'space-before-function-paren':0
+    'space-before-function-paren':0,
+    'quote-props':0,
+    'vue/no-unused-vars':0
   },
 
   'extends': [
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,149 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/mizuho.png" alt />
+        <span>商品后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="iscollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#8b090f"
+          text-color="#fff"
+          active-text-color="#ff6a04"
+          unique-opened
+          :collapse="iscollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模版区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/'+subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavstate('/'+subItem.path)"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-s-promotion"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        '125': 'el-icon-s-custom',
+        '103': 'el-icon-message-solid',
+        '101': 'el-icon-goods',
+        '102': 'el-icon-s-order',
+        '145': 'el-icon-s-data'
+      },
+      //   是否折叠
+      iscollapse: false,
+      //   被激活的链接地址
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
       console.log('sdff')
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.axios.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮，切换菜单的折叠与展示
+    toggleCollapse() {
+      this.iscollapse = !this.iscollapse
+    },
+    // 保存链接的激活状态
+    saveNavstate(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #b31b1c;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #8b090f;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #f9f9fa;
+}
+.toggle-button {
+  background-color: #8b090f;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: right;
+  padding-right: 22px;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
