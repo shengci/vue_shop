49acbf1e4cb89aafb069379bb2854da6b71938be
boyhoodbaby <boyhoodbaby@163.com>
2020-08-15 10:56:05 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,8 @@ import {
   Button, Form, FormItem, Input, message, Container, Header, Aside, Main, Menu,
   Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col,
   Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree,
-  Select, Option, Cascader, Alert, Tabs, TabPane
+  Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup,
+  Checkbox, Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -37,5 +38,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
+
 Vue.prototype.$message = message
 Vue.prototype.$confirm = MessageBox.confirm
